343ac0a151b56380a58c6ba6db20e1c5c68df794
Jeferson <jefersonpeter32@gmail.com>
2021-10-31 20:59:57 -0300

    Tratamento de números, que apareceriam como texto e não conseguia-se fazer os processos seguintes

--- src/alarme.py
+++ src/alarme.py
@@ -4,6 +4,23 @@ agendar = sched.scheduler(time.time, time.sleep)
 
 def soar():
     playsound("sinos.wav")
+    
+def verificar_segundos(palavras):
+    word = palavras
+    if word == 'um': return 1
+    if word == 'dois':
+        print(f'if word igual a dois : {word}')
+        return 2
+    if word == 'três' or word == "tres": return 3
+    if word == 'quatro': return 4
+    if word == 'cinco': return 5
+    if word == 'seis': return 6
+    if word == 'sete': return 7
+    if word == 'oito': return 8
+    if word == 'nove': return 9
+    if word == 'dez': return 10
+    
+    
 
 def definir(frase):
     repitir=False
@@ -11,14 +28,18 @@ def definir(frase):
     for palavra in palavras:
         if palavra == 'segundos':
             tempo = palavras[palavras.index('segundos') - 1]
+            print(f'antes verificar tempo: {tempo}')
+            print(type(tempo))
+            if tempo.isalpha():
+                tempo = verificar_segundos(tempo)
+                print(f'tempo: {tempo}')
         if palavra == 'minutos':
-            #tempo = (palavras[palavras.index('minutos') - 1]) * (60)
-            pass
+            tempo = (palavras[palavras.index('minutos') - 1]) * (60)
         if palavra == 'horas':
-            #tempo = (palavras[palavras.index('horas') - 1]) * (60 * 60)
-            pass
-        if palavra == 'repitir':
+            tempo = (palavras[palavras.index('horas') - 1]) * (60 * 60)
+        if palavra == 'repitir' or palavra == 'repetir':
             repitir=True
+    print(f"tempo: {tempo}")
     agendar.enter(int(tempo), 1, soar)
     agendar.run()
     return("Assitente: Alarme agendado")
