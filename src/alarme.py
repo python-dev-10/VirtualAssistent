8fc8c4039e819305fec6aff272bf2077f774782d
Jeferson <jefersonpeter32@gmail.com>
2021-10-31 21:04:22 -0300

    Retirada de logs em código

--- src/alarme.py
+++ src/alarme.py
@@ -8,9 +8,7 @@ def soar():
 def verificar_segundos(palavras):
     word = palavras
     if word == 'um': return 1
-    if word == 'dois':
-        print(f'if word igual a dois : {word}')
-        return 2
+    if word == 'dois': return 2
     if word == 'três' or word == "tres": return 3
     if word == 'quatro': return 4
     if word == 'cinco': return 5
@@ -20,26 +18,20 @@ def verificar_segundos(palavras):
     if word == 'nove': return 9
     if word == 'dez': return 10
     
-    
-
 def definir(frase):
     repitir=False
     palavras=frase.split()
     for palavra in palavras:
         if palavra == 'segundos':
             tempo = palavras[palavras.index('segundos') - 1]
-            print(f'antes verificar tempo: {tempo}')
-            print(type(tempo))
             if tempo.isalpha():
                 tempo = verificar_segundos(tempo)
-                print(f'tempo: {tempo}')
         if palavra == 'minutos':
             tempo = (palavras[palavras.index('minutos') - 1]) * (60)
         if palavra == 'horas':
             tempo = (palavras[palavras.index('horas') - 1]) * (60 * 60)
         if palavra == 'repitir' or palavra == 'repetir':
             repitir=True
-    print(f"tempo: {tempo}")
     agendar.enter(int(tempo), 1, soar)
     agendar.run()
     return("Assitente: Alarme agendado")
